a606923a7093598b5d434274022c6fab3f39b9ad
Aaron Lott <aarontlott@gmail.com>
2019-09-07 15:43:36 +0100

    FEAT (Specific Forces) More data on the forces
    Component-ising things, and setting up the specific Force adapter.

--- app/routes/neighbourhood.js
+++ app/routes/neighbourhood.js
@@ -2,7 +2,12 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 	model(params){
-		return this.store.query('forcesNeighbourhood', { id: params.id })
+		var forceName = this.store.queryRecord('specificForce', { id: params.id } );
+		var retArray = {"force":"","neighbourhoods":""};
+		retArray.force = forceName;
+		retArray.neighbourhoods = this.store.query('forcesNeighbourhood', { id: params.id });
+		return retArray;
+		// return this.store.query('forcesNeighbourhood', { id: params.id });
 	},
 
 	setupController(controller, model) {
