426b39ee9022a7fa9908f13839e47982f6ee2d4f
Aaron Lott <aarontlott@gmail.com>
2019-09-09 10:39:39 +0100

    FEAT (Polishing) Final bits

--- app/components/neighbourhood-event.js
+++ app/components/neighbourhood-event.js
@@ -23,7 +23,6 @@ export default Component.extend({
 					cards[i].classList.remove('d-none', 'd-block');
 
 					var date = new Date(cards[i].querySelector('.card .start_date').innerText);
-					console.log(date);
 					if (date > startDate && date < endDate) {
 						if (!cards[i].classList.contains('d-block')) {
 							cards[i].classList.toggle('d-block');
@@ -40,8 +39,37 @@ export default Component.extend({
 				}
 			}
 		},
-		filterByType() {
-			
+		filterByType(filterTarget) {
+			var reset = document.querySelectorAll('.type_buttons .btn');
+			for (var i = reset.length - 1; i >= 0; i--) {
+				if (reset[i].classList.contains('active')) {
+					reset[i].classList.remove('active');
+				}
+			}
+			var cards = document.querySelectorAll('.card');
+			var filterValue = document.querySelector('.type_buttons .' + filterTarget);
+			var searchVal = filterTarget;
+			if (!filterValue.classList.contains('active')) {
+				filterValue.classList.add('active');
+				for (var i = cards.length - 1; i >= 0; i--) {
+					cards[i].classList.remove('d-none', 'd-block');
+					var title = cards[i].querySelector('.event_type').innerText.toLowerCase();
+					if (title.indexOf(searchVal) !== -1) {
+						if (!cards[i].classList.contains('d-block')) {
+							cards[i].classList.toggle('d-block');
+						}
+					} else {
+						if (!cards[i].classList.contains('d-none')) {
+							cards[i].classList.toggle('d-none');
+						}
+					}
+				}
+			} else {
+				filterValue.classList.remove('active');
+				for (var i = cards.length - 1; i >= 0; i--) {
+					cards[i].classList.remove('d-none', 'd-block');
+				}
+			}
 		}
 	}
 });
